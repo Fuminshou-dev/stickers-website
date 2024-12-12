5541564e669486ce95f2ed740847ae95351060cb
Fuminshou <petrusman0@gmail.com>
2024-12-13 02:06:25 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Prikleika Store",
@@ -19,6 +20,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Header />
           {children}
+          <GoogleAnalytics gaId="G-4W5SMYGRJE" />
         </ThemeProvider>
       </body>
     </html>
